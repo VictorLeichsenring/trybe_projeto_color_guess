cdb3627477917420cf3539a85d99e8099bd5aa8a
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-20 16:26:51 -0300

    Criaçao de texto para sinalizar acertos e erros

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 const textRGB = document.getElementById('rgb-color');
 const options = document.getElementById('options');
+const answer = document.getElementById('answer');
 
+function play(event) {
+    const choice = event.target;
+    if (choice.style.backgroundColor === textRGB.innerText) {
+        answer.innerText = 'Acertou!';
+    } else {
+        answer.innerText = 'Errou! Tente novamente!';
+    }
+}
 function generateOptions(quantidade){
+    const positionRandom = generateRandomInt(0, quantidade);
+    const winnerColor = textRGB.innerText;
+    console.log(winnerColor)
     for (let i = 0; i < quantidade; i += 1) {
         const element = document.createElement('div');
         element.classList.add('ball');
-        element.style.backgroundColor = generateColor();
+        if (i === positionRandom) {
+            element.style.backgroundColor = winnerColor
+        } else {
+            element.style.backgroundColor = generateColor();
+        }
         options.appendChild(element);
-    }    
+        element.addEventListener('click', play);
+    }
 }
 
 function generateColor() {
-    const red = generateRandomInt();
-    const green = generateRandomInt();
-    const blue = generateRandomInt();
+    const red = generateRandomInt(0, 255);
+    const green = generateRandomInt(0, 255);
+    const blue = generateRandomInt(0, 255);
     const color = `rgb(${red}, ${green}, ${blue})`;
     return color;
 }
 
-function generateRandomInt() {
-    return Math.floor(Math.random()* (255 - 0) + 0) + 0;
+function generateRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 textRGB.innerText = generateColor();
